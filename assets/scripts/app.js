0efa3de5904247d67c87b9d1ebce7ee6d7c0cef3
3poitn14guy <sunshine.farm.nc@gmail.com>
2017-07-25 14:16:06 -0400

    update to apiOrigins
    added heroku info to config and app files

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,7 +1,6 @@
 'use strict'
-// need to check this as of 11:23 Saturday for correct host
+
 module.exports = {
-  // changed host to local 1:25 saturday
-  host: 'https://localhost/7165/index.html'
-  // host: 'https://aqueous-atoll-85096.herokuapp.com/'
+
+  host: 'https://groovy-movie.herokuapp.com'
 }
